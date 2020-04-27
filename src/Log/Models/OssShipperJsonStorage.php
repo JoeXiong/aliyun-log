6fdec2ef5695d6f7e1fc93dacb814eb6269ded9e
zhangzhongxian <zhangzhongxian@gaodun.com>
2020-04-27 13:33:32 +0800

    init

---
+++ src/Log/Models/OssShipperJsonStorage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright (C) Alibaba Cloud Computing
+ * All rights reserved
+ */
+namespace AliyunLog;
+class Aliyun_Log_Models_OssShipperJsonStorage extends Aliyun_Log_Models_OssShipperStorage{
+
+    public function to_json_object(){
+        return array();
+    }
+}
